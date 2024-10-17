6bebefbe6c8930be497d149506e59aa1f2681d1a
claudio <claudiosilva@steelchunk.eu>
2024-10-17 17:23:53 +0100

    updated some scripts

---
+++ assets/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    //public GameManager gameManager = GameManager.Idle;
+    //private List<IGameStateController> gameStateControllers = new List<IGameStateController>();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        //gameStateControllers.AddRange(FindObjectOfType<MonoBehaviour>().OfType<IGameStateController>());
+    }
+
+    private void Update()
+    {
+        // foreach (var controller in gameStateControllers)
+        // {
+        //     switch (gameManager)
+        //     {
+        //         case GameManager.Idle:
+        //             controller.Idle();
+        //             break;
+        //     }
+        // }
+    }
+
+}
